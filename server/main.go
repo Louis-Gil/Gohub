f17c77e26c476a24cc2fb6d6707d447c7e7855e3
Louis-Gil <lotusnate@gmail.com>
2023-10-24 23:37:20 +0900

    feat : Add server http1.1 and http3

--- server/main.go
+++ server/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/quic-go/quic-go/http3"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ok",
-		})
+		c.String(200, "Hello, HTTP/3!")
 	})
-	r.Run()
+	return r
+}
+
+func main() {
+	router := setupRouter()
+
+	go func() {
+		if err := router.Run(":8080"); err != nil {
+			log.Fatalf("Failed to run server: %v", err)
+		}
+	}()
+
+	server := &http3.Server{
+		Addr: ":8081",
+		Handler: router,
+	}
+
+	if err := server.ListenAndServeTLS("./../localhost.crt", "./../localhost.key"); err != nil {
+		log.Fatalf("Failed to run Http/3 server: %v", err)
+	}
 }
